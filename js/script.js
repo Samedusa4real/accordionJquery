3b1543b494bdf587575cbe52a677225417dc7c47
Samedusa4real <samadnm@code.edu.az>
2023-04-01 12:25:10 -0800

    accordion logic compleeted successfully

--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
-// const itemButtons = document.querySelectorAll(".accordion-item .accordion-header i")
-// const itemContents = document.querySelectorAll(".accordion-item .accordion-content")
+const itemButtons = document.querySelectorAll(".accordion-item .accordion-header i")
+const itemContents = document.querySelectorAll(".accordion-item .accordion-content")
 
-// itemButtons.forEach(itemButton => {
-//     itemButton.addEventListener("click", function () {
-//         const item = itemButton.parentNode.parentNode
-//         item.classList.toggle("active")
-//     })
-// })
+itemButtons.forEach(itemButton => {
+    itemButton.addEventListener("click", function () {
+        const item = itemButton.parentNode.parentNode
+        const accordionItems = Array.from(item.parentNode.children)
 
-$(document).ready(function(){
-    const itemButtons = $(".accordion-item .accordion-header i")
-    const itemContents = $(".accordion-item .accordion-content")
+        item.classList.toggle("active")
 
-    itemButtons.each(function(){
-        $(this).click(function(){
-            const item = $(this).parent().parent()
-            item.toggleClass("active")
+        accordionItems.forEach(accordionItem => {
+            const accordionText = accordionItem.lastElementChild
+            if (!accordionItem.classList.contains("active")) {
+                console.log(accordionItem);
+                accordionText.style.display = "none"
+            }
+            else{
+                accordionText.style.display = "block"
+            }
+            accordionItem.classList.remove("active")
         })
     })
 })
+
+// $(document).ready(function(){
+//     const itemButtons = $(".accordion-item .accordion-header i")
+//     const itemContents = $(".accordion-item .accordion-content")
+
+//     itemButtons.each(function(){
+//         $(this).click(function(){
+//             const item = $(this).parent().parent()
+//             const content = $(this).parent().next()
+//             item.toggleClass("active")
+            
+//             item.siblings().removeClass("active")
+//         })
+//     })
+// })
